dfc607b582dea7a67d6c57536de5aaf1afeb2620
Mariya <maripolevay@gmail.com>
2026-03-24 18:56:12 +0500

    5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const items =[
+  {
+    "id": 1,
+    "name": "Sasha"
+  },
+  {
+    "id": 2,
+    "name": "Masha"
+  },
+];
+let id = 0;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  const items = [
-    {
-      "id": 1,
-      "name": "Sasha"
-    },
-    {
-      "id": 2,
-      "name": "Masha"
-    },
-  ]
   res.json(items);
 });
+
+router.post('/', function(req, res, next) {
+  const newUser = req.body;
+  items.push({
+    "id": items.length + 1,
+    "name": newUser.name,
+  });
+  res.status(201).json(newUser);
+})
 
 module.exports = router;
